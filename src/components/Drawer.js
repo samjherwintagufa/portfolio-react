c189279908da9f0ab73b730aceafd9fffe3b92d8
samjherwintagufa <sanadosam0@gmail.com>
2024-03-13 08:02:39 +0800

    commit the updates with drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -8,8 +8,14 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import Avatar from '@mui/material/Avatar';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import LanguageIcon from '@mui/icons-material/Language';
+import InfoIcon from '@mui/icons-material/Info';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -20,31 +26,45 @@ export default function TemporaryDrawer() {
 
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+        
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <Avatar alt="Sam Jherwin Tagufa" src="src/icons/pic.jpg"
+      style={{ 
+        width: '100px', // Adjust size as needed
+        height: '100px', // Adjust size as needed
+        margin: 'auto', // Centers the avatar horizontally
+      }}  />
+  {['Curriculum Vitae', 'Educational achievements', 'Programming Languages', 'Web Dev'].map((text, index) => (
+    <ListItem key={text} disablePadding>
+      <ListItemButton>
+        <ListItemIcon>
+          {index === 0 ? <AssessmentIcon /> :
+           index === 1 ? <WorkspacePremiumIcon /> :
+           index === 2 ? <TerminalIcon /> :
+           <LanguageIcon />}
+        </ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </ListItem>
+  ))}
+</List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+  {['About me', 'Past projects', 'Contacts'].map((text, index) => (
+    <ListItem key={text} disablePadding>
+      <ListItemButton>
+        <ListItemIcon>
+          {index === 0 ? <InfoIcon /> :
+           index === 1 ? <AccountTreeIcon /> :
+           index === 2 ? <PermContactCalendarIcon /> :
+           null}
+        </ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </ListItem>
+  ))}
+</List>
+
     </Box>
   );
 
